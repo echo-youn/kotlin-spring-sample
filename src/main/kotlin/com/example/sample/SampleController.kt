6a061e18357225e4f3109f3c86ca97373fae98c7
echo <echo@nexon.co.kr>
2023-11-03 16:57:10 +0900

    add samples

--- src/main/kotlin/com/example/sample/SampleController.kt
+++ src/main/kotlin/com/example/sample/SampleController.kt
@@ -2,6 +2,7 @@ package com.example.sample
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -9,5 +10,13 @@ class SampleController {
     @GetMapping("/")
     fun index(): ResponseEntity<String> {
         return ResponseEntity.ok("hello World")
+    }
+
+    @GetMapping("/test2")
+    fun index2(
+        @RequestParam param1: Int,
+        @RequestParam param2: Int?
+    ): String {
+        return "$param1${param2}"
     }
 }
